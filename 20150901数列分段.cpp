6dce4a9be87aaabc7f648732ef2866d4d21a0a1b
Fuguidan <32536958+Fuguidan@users.noreply.github.com>
2020-02-06 23:34:16 +0800

    Add files via upload

---
+++ 20150901数列分段.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n], i, count = 1;
+	for(i = 0; i < n; i++)
+	{
+		cin>>a[i];
+	}
+	for(i = 1; i < n; i++)
+	{
+		if(a[i] != a[i-1])
+		{
+			count++;
+		}
+	}
+	cout<<count;
+}
